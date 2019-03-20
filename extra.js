5873aed8ea8ff4f70fd83c57831d4dc1c0f5cfc7
CodeWeaverCW <codeweaver@loganhall.net>
2019-03-19 20:14:11 -0500

    Added JSC processing

--- extra.js
+++ extra.js
@@ -8,12 +8,25 @@ const fs = require('fs');
 
 
 /* Process */
+function interpolate_snippet(exa_code) {
+
+}
+
 // One file at a time (each file more or less represents an EXA)
 for (let f=2; f<process.argv.length; f++) {
 	let exa_code = fs.readFileSync(process.argv[f], 'utf8');
 	let regexp;
+	let captures;
 
-	// Scan entire file for each macro directive
-	regexp = /@NOP\s+(\d+)/gi;
-	exa_code.replace(matches=regexp, "");
+	/* Scan entire file for each macro directive, in given order */
+	// JSC (JavaScript Code)
+	regexp = /^@JSC\s*(?:^([^]*?)^@END|(.+)$)/gm;
+	while (captures = regexp.exec(exa_code))
+		eval(captures[0]);
+
+	// REP (repetitions)
+	regexp = /^@REP\s*(?:\{(.+)\}|(\S+))\s*(?:^([^]*?)^@END|(.+)$)/gm;
+	while (captures = regexp.exec(exa_code)) {
+		//
+	}
 }
